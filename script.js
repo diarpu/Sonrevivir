6e1fb7e268551f4fc73bfae63f6c6e226caa53d8
Diego Paz <diegoparancib@gmail.com>
2021-08-11 14:30:44 -0300

    dm2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function registerSW() {
   if ('serviceWorker' in navigator) {
     try {
-      await navigator.serviceWorker.register('sw.js');
+      await navigator.serviceWorker.register('./sw.js');
     } catch (e) {
       console.log(`SW registration failed`);
     }
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ;
 //asignar un nombre y versión al cache
-const cacheName = 'v1_cache_sonrevivir';
-const staticAssets = [
+const CACHE_NAME = 'v1_cache_sonrevivir';
+const urlsToCache = [
     './',
     'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css',
     'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css',
@@ -13,7 +13,8 @@ const staticAssets = [
     'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js',
 
     './dist/css/style.min.css',
-    './dist/js/main3813.js',
+    './dist/js/main.js',
+    './script.js',
     './dist/fonts',
     './dist/images/cta-fig-1.jpg',
     './dist/images/hero-prop-10.jpg',
@@ -25,41 +26,51 @@ const staticAssets = [
     './dist/images/services-h-fig.jpg',
   ];
 
-  self.addEventListener('install', async e => {
-    const cache = await caches.open(cacheName);
-    await cache.addAll(staticAssets);
-    return self.skipWaiting();
-  });
-  
-  self.addEventListener('activate', e => {
-    self.clients.claim();
-  });
-  
-  self.addEventListener('fetch', async e => {
-    const req = e.request;
-    const url = new URL(req.url);
-  
-    if (url.origin === location.origin) {
-      e.respondWith(cacheFirst(req));
-    } else {
-      e.respondWith(networkAndCache(req));
-    }
-  });
-  
-  async function cacheFirst(req) {
-    const cache = await caches.open(cacheName);
-    const cached = await cache.match(req);
-    return cached || fetch(req);
-  }
-  
-  async function networkAndCache(req) {
-    const cache = await caches.open(cacheName);
-    try {
-      const fresh = await fetch(req);
-      await cache.put(req, fresh.clone());
-      return fresh;
-    } catch (e) {
-      const cached = await cache.match(req);
-      return cached;
-    }
-  }
+//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+          .then(() => self.skipWaiting())
+      })
+      .catch(err => console.log('Falló registro de cache', err))
+  )
+})
+
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE_NAME]
+
+  e.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            //Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName)
+            }
+          })
+        )
+      })
+      // Le indica al SW activar el cache actual
+      .then(() => self.clients.claim())
+  )
+})
+
+//cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          //recuperar del cache
+          return res
+        }
+        //recuperar de la petición a la url
+        return fetch(e.request)
+      })
+  )
+})
